73fd550873e1575bbf79a91281cc806dc8046c36
NicoleBorbe <nicole.borbe@gmail.com>
2022-08-18 12:49:19 +0200

    version1

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+document.getElementById("info").innerHTML = "<h1>Hello World</h1>";
+document.getElementById("h2").innerHTML = "<h2>How Are You?</h2>";
+document.getElementById("container").innerHTML = "<p>start of the element</p>";
+document.write("end of the element");
